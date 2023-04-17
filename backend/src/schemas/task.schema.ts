e69c719396280f81a58e21c12e7bbcc1c5053fba
Hannes Ziereis <hannes.ziereis@gmail.com>
2023-04-17 19:38:43 +0200

    add task schema

---
+++ backend/src/schemas/task.schema.ts
@@ -0,0 +1,61 @@
+import { IsBoolean, IsDate, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+
+class TaskSchema {
+
+  @IsNumber()
+  id: number
+
+  @IsNotEmpty()
+  title: string;
+
+  @IsBoolean()
+  isChecked: boolean
+
+  @IsString()
+  description: string;
+  
+  @IsDate()
+  dueDate: Date;
+
+  constructor() {
+    this.isChecked = false;
+  }
+}
+
+export class CreateTaskSchema extends TaskSchema {
+  constructor(title: string, description: string, dueDate: Date) {
+    super();
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+  }
+}
+
+export class UpdateTaskSchema extends TaskSchema {
+  @IsOptional()
+  @IsNotEmpty()
+  title: string;
+
+  @IsOptional()
+  description: string;
+
+  @IsOptional()
+  dueDate: Date;
+  
+  constructor(title: string, description: string, dueDate: Date) {
+    super()
+    this.title = title
+    this.description = description; 
+    this.dueDate = dueDate;
+  }
+}
+
+export class ReadTaskSchema extends TaskSchema {
+  @IsNumber()
+  id: number;
+
+  constructor(id: number) {
+    super()
+    this.id = id
+  }
+}
