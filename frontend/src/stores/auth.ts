60903ad9e15986cc3d200d687b4d07612bc64e72
Lucas Schießl <lucas.schiessl@hs-augsburg.de>
2023-05-06 11:12:05 +0200

    added local store for auth token so that user stays logged in, as well as logout function to log user out

--- frontend/src/stores/auth.ts
+++ frontend/src/stores/auth.ts
@@ -1,22 +1,28 @@
 import { defineStore } from 'pinia'
 import baseApi from '@/common/base-api.service';
 
+
 export const useAuthStore = defineStore({
   id: "auth",
   state: () => ({
-    jwt: null,
+    jwt: localStorage.getItem('jwt') || null,  
   }),
   actions: {
     async login(email: string, password: string) {
       try {
         const response = await baseApi.post("/auth/login", { email, password })
         this.jwt = response.data
+        localStorage.setItem('jwt', response.data)
         return true
       } catch (error) {
         console.error(error)
         return false
       }
     },
+    logout(){
+      localStorage.removeItem('jwt')
+      this.jwt = null
+    }
   },
   getters: {
     isLoggedIn(): boolean {
