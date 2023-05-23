06dd734f1bd141a52a80d6536ed9e6e26bcc8246
Lucas Schießl <lucas.schiessl@hs-augsburg.de>
2023-05-23 21:35:17 +0200

    removed unused error messages

--- frontend/src/components/AppRegistration.vue
+++ frontend/src/components/AppRegistration.vue
@@ -7,9 +7,7 @@
               <div class="text-center">
                 <h1 class="block text-2xl font-bold text-gray-800">Register</h1>
               </div>
-    
               <div class="mt-5">
-    
                 <!-- Form -->
                 <form @submit.prevent="register">
                   <div class="grid gap-y-4">
@@ -18,35 +16,19 @@
                       <label for="firstName" class="block text-sm mb-2">First Name</label>
                       <div class="relative">
                         <input type="firstName" id="firstName" name="firstName" class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 border" required v-model="firstName">
-                        <div class="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
-                          <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
-                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-                          </svg>
-                        </div>
                       </div>
                     </div>
                     <div>
                       <label for="lastName" class="block text-sm mb-2">Last Name</label>
                       <div class="relative">
                         <input type="lastName" id="lastName" name="lastName" class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 border" required v-model="lastName">
-                        <div class="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
-                          <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
-                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-                          </svg>
-                        </div>
                       </div>
                     </div>
                     <div>
                       <label for="email" class="block text-sm mb-2">Email</label>
                       <div class="relative">
                         <input type="email" id="email" name="email" class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 border" required aria-describedby="email-error" v-model="email">
-                        <div class="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
-                          <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
-                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-                          </svg>
-                        </div>
                       </div>
-                      <p class="hidden text-xs text-red-600 mt-2" id="email-error">This Email Adress has already been taken</p>
                     </div>
                     <!-- End Form Group -->
     
@@ -64,7 +46,6 @@
                           </button>
                         </div>
                       </div>
-                      <p class="hidden text-xs text-red-600 mt-2" id="password-error">8+ characters required</p>
                     </div>
                     <div>
                       <div class="flex justify-between items-center">
