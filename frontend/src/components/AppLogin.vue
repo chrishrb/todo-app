00b6ba11d37d1e51041b98dd3cd1824730ac2e63
Lucas Schießl <lucas.schiessl@hs-augsburg.de>
2023-05-23 21:20:29 +0200

    removed some more unused errors

--- frontend/src/components/AppLogin.vue
+++ frontend/src/components/AppLogin.vue
@@ -24,11 +24,6 @@
                   <label for="email" class="block text-sm mb-2">Email</label>
                   <div class="relative">
                     <input type="email" id="email" name="email" class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 border" required aria-describedby="email-error" v-model="email">
-                    <div class="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
-                      <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
-                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-                      </svg>
-                    </div>
                   </div>
                 </div>
                 <!-- End Form Group -->
