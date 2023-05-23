fe3a02ffaed92eb8de4b56787a165d021e0f2352
Lucas Schießl <lucas.schiessl@hs-augsburg.de>
2023-05-23 20:46:06 +0200

    added registration component

---
+++ frontend/src/views/RegistrationSuccessView.vue
@@ -0,0 +1,7 @@
+<template>
+    <AppRegistrationSuccess/>
+</template>
+  
+<script lang="ts" setup>
+import AppRegistrationSuccess from '@/components/AppRegistrationSuccess.vue'
+</script>
---
+++ frontend/src/views/RegistrationView.vue
@@ -0,0 +1,7 @@
+<template>
+    <AppRegistration/>
+</template>
+  
+<script lang="ts" setup>
+import AppRegistration from '@/components/AppRegistration.vue'
+</script>
