dc4d42ec2fd22bc95465112a3cc944d379cac64e
Hannes Ziereis <hannes.ziereis@gmail.com>
2023-05-31 18:33:26 +0200

    proof of concept

--- frontend/src/components/main/AppTask.vue
+++ frontend/src/components/main/AppTask.vue
@@ -3,8 +3,8 @@
     <div class="flex bg-white border shadow-sm rounded-xl p-4 m-2 items-center">
       <button id="check3"
         class="flex-none w-5 h-5 rounded-full border border-primary-500 cursor-pointer hover:border-primary-600 flex"
-        :class="[{ 'bg-primary-500': item.checked }, 'bg-white']" @click="store.toggleChecked(key)"></button>
-      <div class="pl-2">{{ item.name }}</div>
+        :class="[{ 'bg-primary-500': item.isChecked }, 'bg-white']" @click="store.toggleChecked(key)"></button>
+      <div class="pl-2">{{ item.title }}</div>
     </div>
   </div>
 </template>
@@ -13,4 +13,6 @@
 import { useTaskStore } from '@/stores/tasks';
 
 const store = useTaskStore();
+store.getMine()
+console.log(store.getTasks)
 </script>
