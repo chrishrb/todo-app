fa53e6947de9f11a619be259bf7b346df90b5ee1
Hannes Ziereis <hannes.ziereis@gmail.com>
2023-06-04 17:22:37 +0200

    toggle item on click

--- frontend/src/components/main/AppTask.vue
+++ frontend/src/components/main/AppTask.vue
@@ -1,9 +1,15 @@
 <template>
+
+  <!-- Task list -->
   <div v-for="(item, key) in store.tasks">
-    <div class="flex bg-white border shadow-sm rounded-xl p-4 m-2 items-center">
+    <div class="flex bg-white border shadow-sm rounded-xl p-4 m-2 items-center hover:border-gray-400">
+
       <button id="check3"
-        class="flex-none w-5 h-5 rounded-full border border-primary-500 cursor-pointer hover:border-primary-600 flex"
-        :class="[{ 'bg-primary-500': item.isChecked }, 'bg-white']" @click="store.toggleChecked(key)"></button>
+        class="flex-none w-5 h-5 rounded-full border border-primary-500 cursor-pointer hover:border-primary-800 flex"
+        :class="[{ 'bg-primary-500': item.isChecked }]"
+        @click="store.toggleChecked(item.id)"
+      />
+
       <div class="pl-2">{{ item.title }}</div>
     </div>
   </div>
@@ -11,8 +17,11 @@
 
 <script lang="ts" setup>
 import { useTaskStore } from '@/stores/tasks';
+import { onMounted } from 'vue';
 
 const store = useTaskStore();
-store.getMine()
-console.log(store.getTasks)
+
+onMounted(() => {
+  store.getMine()
+})
 </script>
