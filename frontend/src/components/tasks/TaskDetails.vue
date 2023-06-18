6349ffb48a20bca9907da6a11c8f1071eaa09ff5
Hannes Ziereis <hannes.ziereis@gmail.com>
2023-06-18 22:02:16 +0200

    38 make description scrollable

--- frontend/src/components/tasks/TaskDetails.vue
+++ frontend/src/components/tasks/TaskDetails.vue
@@ -1,17 +1,15 @@
 <template>
 
   <Teleport to='#modal'>
-    <div class="hs-overlay fixed top-0 left-0 z-[60] justify-center w-full h-full bg-black bg-opacity-10 overflow-x-hidden overflow-y-auto items-center" v-if="isModalOpen">
+    <div class="hs-overlay fixed top-0 left-0 z-[60] justify-center w-full h-full bg-black bg-opacity-10 overflow-hidden" v-if="isModalOpen">
       <!-- <div ref="modal" class="hs-overlay-open:opacity-100 hs-overlay-open:duration-500 transition-all ease-out sm:max-w-lg sm:w-full m-3"> -->
-      <div ref="modal" class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-1000 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto mt-[10%] flex items-center">
-    <!-- <div v-if="isModalOpen" class="hs-overlay hidden w-full h-full fixed top-0 left-0 z-[60] overflow-x-hidden overflow-y-auto [--overlay-backdrop:static] items-center"> -->
-      <!-- <div ref="modal" class="hs-overlay-open:opacity-100 hs-overlay-open:duration-500 opacity-0 transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto pointer-events-auto"> -->
-        <router-view>
-          <div class="flex flex-col bg-white border shadow-sm rounded-xl">
-            <div class="flex justify-between items-center py-3 px-4 border-b">
-              <h3 class="font-bold text-gray-800">
+      <router-view>
+      <div ref="modal" class="sm:w-[60%] m-3 sm:mx-auto mt-[10%] flex items-center h-min">
+          <div class="flex flex-col bg-white border shadow-sm rounded-xl w-full h-96">
+            <div class="flex justify-between items-center py-3 px-4">
+              <h1 class="text-gray-800 text-xl">
                 {{ store.task?.title }}
-              </h3>
+              </h1>
               <button @click="handleClose" class="hs-dropdown-toggle inline-flex flex-shrink-0 justify-center items-center h-8 w-8 rounded-md text-gray-500 hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition-all text-sm">
                 <span class="sr-only">Close</span>
                 <svg class="w-3.5 h-3.5" width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -19,19 +17,32 @@
                 </svg>
               </button>
             </div>
-            <div class="p-4 overflow-y-auto">
-              <p class="mt-1 text-gray-800">
-                This is a wider card with supporting text below as a natural lead-in to additional content.
-              </p>
+            <div class="flex flex-row overflow-y-hidden">
+              <div class="px-4 py-2 overflow-y-auto">
+                <p class="font-bold">
+                  Beschreibung
+                </p>
+                <p class="mt-1 text-gray-800 max-w-lg h-min">
+                  {{ store.task?.description }}
+                </p>
+              </div>
+              <div class="px-4 py-2 self-end">
+                <p class="font-bold">
+                  Fällig
+                </p>
+                <p class="mt-1 text-gray-800">
+                  {{ store.task?.dueDate }}
+                </p>
+              </div>
             </div>
-            <div class="flex justify-end items-center gap-x-2 py-3 px-4 border-t">
-              <button @click="handleDone" type="button" class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm">
+            <div class="flex justify-end items-center py-2 px-4 border-t">
+              <button @click="handleDone" type="button" class="py-2 px-4 inline-flex justify-center items-center rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm">
                 Done
               </button>
             </div>
           </div>
-        </router-view>
       </div>
+      </router-view>
     </div>
     <!--   </div> -->
     <!-- </div> -->
@@ -80,7 +91,7 @@ async function handleClose() {
 }
 
 function handleDone() { 
-  store.setDone(taskId)
+  store.setDone()
     .then(() => {
       handleClose();
     })
