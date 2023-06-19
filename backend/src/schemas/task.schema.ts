700c3a9d290d57fadc4d6dfdb25e53a6d17a01ae
Lucas Schießl <lucas.schiessl@hs-augsburg.de>
2023-06-19 15:05:18 +0200

    added more error fixing

--- backend/src/schemas/task.schema.ts
+++ backend/src/schemas/task.schema.ts
@@ -64,8 +64,8 @@ export class CreateTaskSchema {
 
   @IsOptional({
     context: {
-      errorCode: ResponseError.TASK_INVALID_DESCRIPTION.errorCode,
-      errorMessage: ResponseError.TASK_INVALID_DESCRIPTION.errorMessage
+      errorCode: ResponseError.TAG_INVALID_TAGNAME.errorCode,
+      errorMessage: ResponseError.TAG_INVALID_TAGNAME.errorMessage
     }
   })
   @IsString({
@@ -139,8 +139,8 @@ export class CreateTaskMeSchema {
 
   @IsOptional({
     context: {
-      errorCode: ResponseError.TASK_INVALID_DESCRIPTION.errorCode,
-      errorMessage: ResponseError.TASK_INVALID_DESCRIPTION.errorMessage
+      errorCode: ResponseError.TAG_INVALID_TAGNAME.errorCode,
+      errorMessage: ResponseError.TAG_INVALID_TAGNAME.errorMessage
     }
   })
   @IsString({
@@ -214,8 +214,8 @@ export class UpdateTaskSchema {
 
   @IsOptional({
     context: {
-      errorCode: ResponseError.TASK_INVALID_IS_CHECKED.errorCode,
-      errorMessage: ResponseError.TASK_INVALID_IS_CHECKED.errorMessage
+      errorCode: ResponseError.TAG_INVALID_TAGNAME.errorCode,
+      errorMessage: ResponseError.TAG_INVALID_TAGNAME.errorMessage
     }
   })
   @IsBoolean({
