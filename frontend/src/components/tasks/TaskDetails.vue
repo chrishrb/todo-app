3159b460cf657c9b4c97cfff5bb1816ef104509a
Hannes Ziereis <hannes.ziereis@gmail.com>
2023-07-02 14:42:59 +0200

    50 add task edit form

--- frontend/src/components/tasks/TaskDetails.vue
+++ frontend/src/components/tasks/TaskDetails.vue
@@ -1,49 +1,64 @@
 <template>
   <teleport to='body'>
     <div class="hs-overlay fixed top-0 left-0 z-[60] justify-center w-full h-full bg-black bg-opacity-10 overflow-hidden" v-show="isModalOpen">
-        <div ref="modal" class="sm:w-[60%] m-3 sm:mx-auto mt-[10%] flex items-center h-min">
-          <div class="flex flex-col bg-white border shadow-sm rounded-xl w-full h-[30rem]">
-            <div class="flex justify-between items-center py-3 px-4">
-              <div class="flex justify-between items-center flex-row">
-                <h1 class="text-2xl text-primary-600">
-                  {{ store.task?.title }}
-                </h1>
-              </div>
+      <div ref="modal" class="sm:w-[60%] m-3 sm:mx-auto mt-[10%] flex items-center h-min">
+
+        <TaskEditForm
+          :task="task!"
+          :close-edit="closeEdit"
+          v-on:save-edit="handleSave"
+          v-if="isEditing"
+        />
+        <div v-else class="flex flex-col bg-white border shadow-sm rounded-xl w-full h-[30rem]">
+          <div class="flex justify-between items-center py-3 px-4">
+            <div class="flex justify-between items-center flex-row">
+              <h1 class="text-2xl text-primary-600">
+                {{ task?.title }}
+              </h1>
+            </div>
+            <div class="inline-flex justify-between items-center">
+              <button
+                class="mr-2 inline-flex flex-shrink-0 justify-center items-center h-8 w-8 rounded-md text-gray-500 hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition-all text-sm"
+                @click.stop="isEditing = true"
+              >
+                <PencilSquareIcon class="w-6 h-6" />
+              </button>
               <button @click="handleClose" class="hs-dropdown-toggle inline-flex flex-shrink-0 justify-center items-center h-8 w-8 rounded-md text-gray-500 hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition-all text-sm">
                 <span class="sr-only">{{ $t('close') }}</span>
-              <XMarkIcon/>
+                <XMarkIcon/>
               </button>
             </div>
-              <div
-                class="px-4 py-2 self-start"
-                :class="[isTaskDue(store.task?.dueDate) ? 'text-red-500' : '']"
-                v-show="store.task?.dueDate">
-              <div class="flex items-center">
-                <CalendarIcon class="h-5 w-5 mr-1"/>
-                <p class="font-bold"> {{ $t('due') }} </p>
-              </div>
-                <p class="mt-1">
-                  {{ getFancyDateString(store.task?.dueDate, locale)}}
-                </p>
-              </div>
-            <div class="flex overflow-y-hidden pr-3 h-full">
-              <div class="px-4 py-2 overflow-y-auto w-full">
+          </div>
+          <div
+            class="px-4 py-2 self-start"
+            :class="[isTaskDue(store.task?.dueDate) ? 'text-red-500' : '']"
+            v-show="store.task?.dueDate">
+            <div class="flex items-center">
+              <CalendarIcon class="h-5 w-5 mr-1"/>
+              <p class="font-bold"> {{ $t('due') }} </p>
+            </div>
+            <p class="mt-1">
+              {{ getFancyDateString(task?.dueDate, locale)}}
+            </p>
+          </div>
+          <div class="flex overflow-y-hidden pr-3 h-full">
+            <div class="px-4 py-2 overflow-y-auto w-full">
               <div class="flex items-center">
                 <ChatBubbleLeftRightIcon class="h-5 w-5 mr-1"/>
                 <p class="font-bold"> {{ $t('description') }} </p>
               </div>
-                <p class="mt-1 text-gray-800 h-min w-full">
-                  {{ store.task?.description }}
-                </p>
-              </div>
-            </div>
-            <div class="flex justify-end items-center py-2 px-4 border-t">
-              <button @click="handleDone" type="button" class="py-2 px-4 inline-flex justify-center items-center rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm">
-                {{ $t('done') }}
-              </button>
+              <span class="mt-1 text-gray-800 h-min w-full">
+                {{ task?.description }}
+              </span>
             </div>
           </div>
+          <div class="flex justify-end items-center py-2 px-4 border-t">
+            <button @click="handleDone" type="button" class="py-2 px-4 inline-flex justify-center items-center rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm">
+              {{ $t('done') }}
+            </button>
+          </div>
         </div>
+      </div>
     </div>
   </teleport>
 </template>
@@ -56,23 +71,28 @@ import { onBeforeRouteUpdate, useRouter } from 'vue-router';
 import { getFancyDateString } from '../utils/formatter';
 import { useI18n } from 'vue-i18n';
 import { XMarkIcon } from "@heroicons/vue/24/solid"
-import { ChatBubbleLeftRightIcon, CalendarIcon } from "@heroicons/vue/24/outline"
+import { ChatBubbleLeftRightIcon, CalendarIcon, PencilSquareIcon} from "@heroicons/vue/24/outline"
 import { isTaskDue } from '../utils/helpers';
+import TaskEditForm from '@/components/tasks/TaskEditForm.vue';
+import type { Task } from '@/schemas/task.schema';
 
 const { locale } = useI18n();
 const store = useTaskStore();
 const router = useRouter();
 
+const isEditing = ref(false);
+
 const modal = ref(null)
 const isModalOpen = ref(false)
 
 const taskId = ref(router.currentRoute.value.params.taskId) as Ref<string>;
-
+const task = ref(store.task);
 
 onMounted(() => {
   if (taskId.value && !isModalOpen.value) {
     return store.fetchTask(taskId.value)
       .then((res) => {
+        task.value = res;
         isModalOpen.value = true
       })
   }
@@ -82,6 +102,7 @@ onBeforeRouteUpdate(async (to, from) => {
   if (to.params.taskId !== from.params.taskId && to.params.taskId) {
     return store.fetchTask(to.params.taskId as string)
       .then((res) => {
+        task.value = res;
         isModalOpen.value = true
       })
   }
@@ -91,8 +112,15 @@ onClickOutside(modal, () => {handleClose()})
 
 async function handleClose() {
   isModalOpen.value = false;
+  isEditing.value = false
   const parentRoute = router.currentRoute.value.matched.length >= 2 ? router.currentRoute.value.matched.slice(-2).shift() : router.currentRoute.value;
   await router.push(parentRoute!)
+}
+
+function handleSave(newTask: Task) {
+  console.log("save", newTask)
+  task.value = newTask;
+  isEditing.value = false;
 }
 
 function handleDone() { 
@@ -101,4 +129,9 @@ function handleDone() {
       handleClose();
     })
 }
+
+function closeEdit() {
+  isEditing.value = false;
+}
+
 </script>
---
+++ frontend/src/components/tasks/TaskEditForm.vue
@@ -0,0 +1,116 @@
+<template>
+
+
+  <div class="flex flex-col bg-white border shadow-sm rounded-xl w-full h-[30rem]">
+    <div class="flex justify-between items-center py-3 px-4">
+      <div class="flex justify-between items-center flex-row">
+        <input class="text-2xl text-primary-600 px-2 block w-full border border-gray-300 rounded-md h-full"
+          v-model="taskDraft.title"
+        />
+      </div>
+      <div class="inline-flex justify-between items-center">
+        <button @click="props.closeEdit" class="hs-dropdown-toggle inline-flex flex-shrink-0 justify-center items-center h-8 w-8 rounded-md text-gray-500 hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition-all text-sm">
+          <span class="sr-only">{{ $t('close') }}</span>
+          <XMarkIcon/>
+        </button>
+      </div>
+    </div>
+    <!-- TODO conditional -->
+    <div
+      class="px-4 py-2 self-start"
+      :class="[false ? 'text-red-500' : '']"
+    >
+      <div class="flex items-center">
+        <CalendarIcon class="h-5 w-5 mr-1"/>
+        <p class="font-bold"> {{ $t('due') }} </p>
+      </div>
+      <VueDatePicker
+        class="mt-1"
+        v-model="taskDraft.dueDate"
+        inline-with-input
+        :auto-apply="true"
+        :min-date="new Date()"
+      />
+    </div>
+    <div class="flex overflow-y-hidden pr-3 h-full">
+      <div class="flex flex-col px-4 py-2 w-full">
+        <div class="flex items-center">
+          <ChatBubbleLeftRightIcon class="h-5 w-5 mr-1"/>
+          <p class="font-bold"> {{ $t('description') }} </p>
+        </div>
+        <textarea class="mt-1 text-gray-800 my-2 py-1 px-2 w-full flex-auto overflow-y-auto border border-gray-300 rounded-md"
+          v-model="taskDraft.description as string"
+        />
+      </div>
+    </div>
+    <div class="flex justify-end items-center py-2 px-4 border-t">
+      <button
+        @click="save()"
+        type="button"
+        class="mr-2 py-2 px-4 inline-flex justify-center items-center rounded-md border border-transparent font-semibold text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm"
+        :class="[isSaveDisabled ? 'bg-gray-500 hover:bg-gray-600' : 'bg-blue-500 hover:bg-blue-600']"
+        :disabled="isSaveDisabled"
+      >
+        Speichern
+      </button>
+      <button @click="cancel()" type="button" class="py-2 px-4 inline-flex justify-center items-center rounded-md border border-transparent font-semibold text-black bg-gray-300 hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm">
+        Abbrechen
+      </button>
+    </div>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import { ref, watch, watchEffect } from 'vue';
+import { useTaskStore } from '@/stores/tasks';
+import {XMarkIcon } from "@heroicons/vue/24/outline";
+import { ChatBubbleLeftRightIcon, CalendarIcon } from "@heroicons/vue/24/outline"
+import type { Task } from '@/schemas/task.schema';
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+const store = useTaskStore();
+
+const isSaveDisabled = ref(true);
+
+const props = defineProps<{
+  task: Task,
+  closeEdit: Function
+}>();
+
+const taskDraft = ref(JSON.parse(JSON.stringify(props.task)) as Task);
+
+const emit = defineEmits<{
+  (event: 'cancleEdit'): void,
+  (event: 'saveEdit', task: Task): void,
+  (event: 'closeEdit'): void
+}>();
+
+watch(taskDraft.value, () => {
+  // if (hasAnyChanges(props.task, taskDraft)) {
+    isSaveDisabled.value = false;
+  // }
+});
+
+function save() {
+  console.log("DRAFT: ", taskDraft)
+  store.updateTask(props.task.id, taskDraft.value);
+  emit('saveEdit', taskDraft.value);
+  props.closeEdit();
+}
+
+function cancel() {
+  //TODO clear localstorage draft
+  props.closeEdit();
+}
+
+// function hasDiff() {
+//   console.log("DIFF: ", props.task)
+//   console.log(taskDraft.value)
+//   const d = props.task != taskDraft.value;
+//   console.log(d)
+//   return d;
+// }
+
+</script>
